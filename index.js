5a1de0776e3cc203555941bfc83c491be5c9e7c6
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2023-09-29 10:58:16 -0400

    add

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+//dynamicHTML
+
 //constructor cos set up properties- object orient 
-function tShirt(brand, price, size, material, color){
+function tShirt(brand, price, size, material, color,image){
 this.brand = brand;
 this.price = price;
 this.size = size;
 this.material = material;
 this.color = color;
+this.image = image;
 }
-var shirtOne = new tShirt("GAPS",3, "S","cotton", "White" )// this is only one t shirt
-var shirtTwo = new tShirt("Gucci",100, "M","polyester", "Red" )
-var shirtThree = new tShirt("Zara",15, "L","blend", "Pink" )
-var shirtFour = new tShirt("H&M",28, "XL","tri-blend", "Black" )
+var shirtOne = new tShirt("GAPS",3, "S","cotton", "White", "https://images.unsplash.com/photo-1598554793905-075f7b355cd9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2hpdGUlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" )// this is only one t shirt
+var shirtTwo = new tShirt("Gucci",100, "M","polyester", "Red" , "https://images.pexels.com/photos/10153595/pexels-photo-10153595.jpeg?auto=compress&cs=tinysrgb&w=800")
+var shirtThree = new tShirt("Zara",15, "L","blend", "Pink", "https://images.pexels.com/photos/15549906/pexels-photo-15549906/free-photo-of-woman-in-pink-shirt-at-the-seaside.jpeg?auto=compress&cs=tinysrgb&w=800" )
+var shirtFour = new tShirt("H&M",28, "XL","tri-blend", "Black","https://images.pexels.com/photos/18077752/pexels-photo-18077752/free-photo-of-young-woman-looking-over-the-shoulder.jpeg?auto=compress&cs=tinysrgb&w=800" )
 
 var arr = [shirtOne,shirtTwo,shirtThree,shirtFour]
 var fPrice = 0//person enter the price 
@@ -24,40 +27,56 @@ console.log(fPrice)
     for(i=0;i<newArray.length; i++){
         //dynamic HTML create tag
         //for price
-        var p = document.createElement('p')
-        var t1 = document.createTextNode('price' + newArray[i].price)
-        p.appendChild(t1)
-        document.getElementById('controlText').appendChild(p)
 
-//brand
-        var p2 = document.createElement('p')
-        var t2 = document.createTextNode('brand' + newArray[i].brand)
-        p2.appendChild(t2)
-        document.getElementById('controlText').appendChild(p2)
-
-        //material size
-        var p3 = document.createElement('p')
-        var t3 = document.createTextNode('size' + newArray[i].size)
-        p3.appendChild(t3)
-        document.getElementById('controlText').appendChild(p3)
-//color
-        var p4 = document.createElement('p')
-        var t4 = document.createTextNode('color' + newArray[i].color)
-        p4.appendChild(t4)
-        document.getElementById('controlText').appendChild(p4)
-//material
-        var p6 = document.createElement('p')
-        var t6 = document.createTextNode('material' + newArray[i].material)
-        p4.appendChild(t4)
-        document.getElementById('controlText').appendChild(p4)
-
-        console.log(newArray[i].brand)
-        console.log(newArray[i].price)
-        console.log(newArray[i].material)
-        console.log(newArray[i].size)
-        console.log(newArray[i].color)
+            alert('hellooooo')
+            var div = document.createElement('div')
+            div.setAttribute('class','smallBox')
+    
+            var img = document.createElement('img')
+            img.setAttribute('src', newArray[i].image)
+    div.appendChild(img)
+    var divOne = document.createElement('div')
+    divOne.setAttribute('class','controlText')
+    
+    
+            var p = document.createElement('p')
+            var t1 = document.createTextNode('price' + newArray[i].price)
+            p.appendChild(t1)
+      divOne.appendChild(p)
+    
+    //brand
+            var p2 = document.createElement('p')
+            var t2 = document.createTextNode('brand' + newArray[i].brand)
+            p2.appendChild(t2)
+            divOne.appendChild(p2)
+    
+            //material size
+            var p3 = document.createElement('p')
+            var t3 = document.createTextNode('size' + newArray[i].size)
+            p3.appendChild(t3)
+            divOne.appendChild(p3)
+    //color
+            var p4 = document.createElement('p')
+            var t4 = document.createTextNode('color' + newArray[i].color)
+            p4.appendChild(t4)
+            divOne.appendChild(p4)
+    //material
+            var p6 = document.createElement('p')
+            var t6 = document.createTextNode('material' + newArray[i].material)
+            p6.appendChild(t6)
+            divOne.appendChild(p6)
+    
+    div.appendChild(divOne)
+            document.body.appendChild(div)
+    
+            console.log(newArray[i].brand)
+            console.log(newArray[i].price)
+            console.log(newArray[i].material)
+            console.log(newArray[i].size)
+            console.log(newArray[i].color)
+        }
     }
-}
+    
 
 
 
@@ -71,36 +90,51 @@ function filterColor(obj){
 function dataColor(){
     fcolor = (document.getElementById('filterColor4dropdown').value)
     var newArray = arr.filter(filterColor)
-
+console.log(newArray)
+console.log(fcolor)
     for(i=0;i<newArray.length; i++){
         //dynamic HTML create tag
         //for price
+  
+        var div = document.createElement('div')
+        div.setAttribute('class','smallBox')
+
+        var img = document.createElement('img')
+        img.setAttribute('src', newArray[i].image)
+div.appendChild(img)
+var divOne = document.createElement('div')
+divOne.setAttribute('class','controlText')
+
+
         var p = document.createElement('p')
         var t1 = document.createTextNode('price' + newArray[i].price)
         p.appendChild(t1)
-        document.getElementById('controlText').appendChild(p)
+  divOne.appendChild(p)
 
 //brand
         var p2 = document.createElement('p')
         var t2 = document.createTextNode('brand' + newArray[i].brand)
         p2.appendChild(t2)
-        document.getElementById('controlText').appendChild(p2)
+        divOne.appendChild(p2)
 
         //material size
         var p3 = document.createElement('p')
         var t3 = document.createTextNode('size' + newArray[i].size)
         p3.appendChild(t3)
-        document.getElementById('controlText').appendChild(p3)
+        divOne.appendChild(p3)
 //color
         var p4 = document.createElement('p')
         var t4 = document.createTextNode('color' + newArray[i].color)
         p4.appendChild(t4)
-        document.getElementById('controlText').appendChild(p4)
+        divOne.appendChild(p4)
 //material
         var p6 = document.createElement('p')
         var t6 = document.createTextNode('material' + newArray[i].material)
-        p4.appendChild(t4)
-        document.getElementById('controlText').appendChild(p4)
+        p6.appendChild(t6)
+        divOne.appendChild(p6)
+
+div.appendChild(divOne)
+        document.body.appendChild(div)
 
         console.log(newArray[i].brand)
         console.log(newArray[i].price)
